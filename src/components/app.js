daaf7a305cf141a7743491d7413714c76572fca4
JUSTIN JOHNSON <justinjohnson@alpine.local>
2020-03-17 17:49:03 -0600

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>DevCamp React Starter</h1>
+        <h2>React Bootstrap</h2>
+        <p>lorem ipsum b*tches</p>
+              </div>
+    );
+  }
+}
