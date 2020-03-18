34e42225320a82f00210891adc126d110bdb952e
JUSTIN JOHNSON <justinjohnson@alpine.local>
2020-03-18 13:55:10 -0600

    built our first class component for the inputs container, and built an input functional component to render custom inputs

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
 import Header from './header';
+import Card from './card';
 
 class Home extends Component {
     render() {
         return (
             <div className="home">
-                { Header('Bottega Madlibs', 'Fill out the fields below and click and click the Generate button to see the Mad Lib story.') }
+                { Header() }
+                <Card />
             </div>
-        )
+        );
     }
 }
 
